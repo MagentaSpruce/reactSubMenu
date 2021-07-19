a491ce0383f526e2f606f478cc6d6a2a6c395c69
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-19 14:48:44 -0400

    Third commit
    Added hero component and sidebar. Iterated through sidebar twice to gain access to sublink data.

--- src/Hero.js
+++ src/Hero.js
@@ -4,9 +4,30 @@ import { AppProvider } from "./context";
 
 import { useGlobalContext } from "./context";
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeModal } = useGlobalContext();
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed,
+            quisquam.
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
+            repudiandae delectus, debitis reiciendis id quae consequuntur facere
+            at neque veniam error in? Atque sed, voluptatibus nobis vero illo
+            ipsam voluptates!
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;

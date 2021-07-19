cbd55bda55cef74507eea4bffd5c4d06a2ac257f
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-07-19 17:46:54 -0400

    Updates

---
+++ precache-manifest.b443eec2f1856d8998ab2224ab31e96f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e3d8840aaa6974c64eab8610228329",
+    "url": "/reactSubMenu/index.html"
+  },
+  {
+    "revision": "8dc39fc5946c8c66681d",
+    "url": "/reactSubMenu/static/css/main.fbca9421.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/reactSubMenu/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactSubMenu/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc39fc5946c8c66681d",
+    "url": "/reactSubMenu/static/js/main.70a2bd30.chunk.js"
+  },
+  {
+    "revision": "82ced38b57fb636c0545",
+    "url": "/reactSubMenu/static/js/runtime-main.93be9ad6.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/reactSubMenu/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/reactSubMenu/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/reactSubMenu/static/media/phone.e908706c.svg"
+  }
+]);
